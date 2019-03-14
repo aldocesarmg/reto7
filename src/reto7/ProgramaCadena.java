3852570d4ca16d3b4ae5e6823a58506629fb7999
aldocesarmg <aldocesarmg@gmail.com>
2019-03-13 19:43:01 -0600

    Creacion de clase ProgramaCadena y metodos

---
+++ src/reto7/ProgramaCadena.java
@@ -0,0 +1,16 @@
+
+package reto7;
+
+public class ProgramaCadena {
+    private char[] arreglosChars;
+    public String arregloString;
+    public double cantidad;
+    
+    public int MetodoRegresaEntero(char miInicial){
+        return miInicial;
+    }
+    
+    public char MetodoRegresaChar(String miNombre){
+        return 0;
+    }
+}
